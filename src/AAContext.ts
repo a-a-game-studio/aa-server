2c382663f6e5d611fa35fe7e1a5a70d462196849
dev <dev>
2021-11-27 14:43:09 +0400

    FIX  body

--- src/AAContext.ts
+++ src/AAContext.ts
@@ -55,7 +55,7 @@ export class AAContext {
 	param:Record<string, string> = {};
 
 	/** Тело/данные запроса */
-	body:object = {};
+	body:Record<string, any> = {};
 
 	/** Позиция контекста */
 	protected pos = 0;
